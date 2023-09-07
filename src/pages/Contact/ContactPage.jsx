765c526c5adf4a9a0aae54a6ccc6811c476a98bf
magic9701 <magic9701@gmail.com>
2023-09-07 17:04:15 +0800

    feat: add contact page

--- src/pages/Contact/ContactPage.jsx
+++ src/pages/Contact/ContactPage.jsx
@@ -4,10 +4,36 @@ import styles from 'pages/Contact/ContactPage.module.scss'
 // components
 import MainLayout from 'components/Layout/MainLayout'
 
+// photo
+import backgroundMountain from 'assets/photos/background-mountain.png'
+import email from 'assets/icons/email.svg'
+import youtube from 'assets/icons/youtube-2.svg'
+
 export default function ContactPage() {
   return (
-    <MainLayout>
-      <div className={styles.container}></div>
+    <MainLayout dark={true}>
+      <div className={styles.container}>
+        <img src={backgroundMountain} />
+        <div className={styles.infoContainer}>
+          <div className={styles.innerContainer}>
+            <h3 className={styles.welcomeWords}>哈囉，歡迎聯絡我！</h3>
+            <div className={styles.item}>
+              <img src={email} className={styles.email} />
+              <a href="mailto:magic9701@gmail.com">magic9701@gmail.com</a>
+            </div>
+            <div className={styles.item}>
+              <img src={youtube} className={styles.youtube} />
+              <a
+                href="https://www.youtube.com/@NienNienKo/featured"
+                target="_blank"
+                rel="noreferrer"
+              >
+                https://www.youtube.com/@NienNienKo/featured
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </MainLayout>
   )
 }
