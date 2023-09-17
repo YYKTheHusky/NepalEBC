5e775ff69d2e7862d909bd4b13ed1ed98378f627
magic9701 <magic9701@gmail.com>
2023-09-18 05:30:36 +0800

    fix: fix style

--- src/components/Card/BlogCard.jsx
+++ src/components/Card/BlogCard.jsx
@@ -1,9 +1,15 @@
 // scss
 import styles from 'components/Card/BlogCard.module.scss'
+import { useNavigate } from 'react-router-dom'
 
 export default function BlogCard({ data }) {
+  const navigate = useNavigate()
   return (
-    <div className={styles.cardContainer} data-aos="zoom-in-up">
+    <div
+      className={styles.cardContainer}
+      data-aos="zoom-in-up"
+      onClick={() => navigate(`/article/${data.id}`)}
+    >
       <div className={styles.imgContainer}>
         <img src={data.photo} />
       </div>
